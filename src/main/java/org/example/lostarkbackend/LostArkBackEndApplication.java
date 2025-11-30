da4d280372e816f61f58c8f7c6f58d43e6828551
Lazernes <dbsauddn1@gmail.com>
2025-11-30 18:35:28 +0900

    Initial Commit

---
+++ src/main/java/org/example/lostarkbackend/LostArkBackEndApplication.java
@@ -0,0 +1,13 @@
+package org.example.lostarkbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostArkBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LostArkBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/lostarkbackend/LostArkBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.lostarkbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostArkBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
